aa494bd7a4179bb2de4b605b8e675973c7b6fd84
nandhinimano11@gmail.com <nandhinimano11@gmail.com>
2021-07-30 05:08:18 +0530

    Registration

--- src/services/api/register.js
+++ src/services/api/register.js
@@ -1,7 +1,26 @@
-import { common, common_multipart } from './utils/core';
+import { common_multipart } from './utils/core';
 
-export const _createproduct = async (payloads, callbacks) => {
-  console.log(payload);
+export const _buyerRegistration = async (payload, callback) => {
+  try {
+    let request = {
+      reqmethod: 'POST',
+      url: 'users/buyerregister',
+      param: {},
+      reqdata: payload,
+    };
+    common_multipart(request)
+      .then((response) => {
+        response.data.statusCode == 200 &&
+          callback(null, response.data.responseContents);
+      })
+      .catch((error) => {
+        console.log('error', error);
+      });
+  } catch (e) {
+    callback(e.response.data, null);
+  }
+};
+export const _sellerregister = async (payload, callback) => {
   try {
     let request = {
       reqmethod: 'POST',
--- src/services/api/utils/core.js
+++ src/services/api/utils/core.js
@@ -1,14 +1,7 @@
 import Axios from 'axios';
 const BASE_URL = 'http://localhost:5000/api/v1/';
 
-interface request {
-  reqmethod: any;
-  url: string;
-  param?: any | null | undefined;
-  reqdata?: any | null | undefined;
-  file?: boolean;
-}
-export const common = async ({ reqmethod, url, param, reqdata }: request) => {
+export const common = async ({ reqmethod, url, param, reqdata }) => {
   return Axios({
     method: reqmethod,
     url: BASE_URL + url,
@@ -22,12 +15,7 @@ export const common = async ({ reqmethod, url, param, reqdata }: request) => {
     },
   });
 };
-export const common_multipart = async ({
-  reqmethod,
-  url,
-  param,
-  reqdata,
-}: request) => {
+export const common_multipart = async ({ reqmethod, url, param, reqdata }) => {
   return Axios({
     method: reqmethod,
     url: BASE_URL + url,
