1b190326ef1dc2269dbaf01ef2daef30ad0db0ff
nandhinimano11@gmail.com <nandhinimano11@gmail.com>
2021-08-03 18:16:15 +0530

    Updates

---
+++ precache-manifest.0a02cad9bc0d3420f6559789a40fb3f7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "164d95ecba2deff0ff15",
+    "url": "/CartUser_web/static/css/main.056e6fbb.chunk.css"
+  },
+  {
+    "revision": "164d95ecba2deff0ff15",
+    "url": "/CartUser_web/static/js/main.164d95ec.chunk.js"
+  },
+  {
+    "revision": "5ee1b5421e9aa5e84f50",
+    "url": "/CartUser_web/static/js/runtime~main.5ee1b542.js"
+  },
+  {
+    "revision": "4f94ee1a25c467221d98",
+    "url": "/CartUser_web/static/css/2.e9596915.chunk.css"
+  },
+  {
+    "revision": "4f94ee1a25c467221d98",
+    "url": "/CartUser_web/static/js/2.4f94ee1a.chunk.js"
+  },
+  {
+    "revision": "9be7ea02fa3fa41aab068404c70b7f84",
+    "url": "/CartUser_web/static/media/23.9be7ea02.jpg"
+  },
+  {
+    "revision": "d693b5780acde1fded18c05588af4d1e",
+    "url": "/CartUser_web/static/media/24.d693b578.jpg"
+  },
+  {
+    "revision": "3129158302bbb6a9f93acc17c585c8f3",
+    "url": "/CartUser_web/static/media/uplod_icon.31291583.svg"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/CartUser_web/static/media/UploadIcon.1cedb6e9.svg"
+  },
+  {
+    "revision": "4e59f7c5f6275ec64349f95e8e2bdf29",
+    "url": "/CartUser_web/static/media/18.4e59f7c5.jpg"
+  },
+  {
+    "revision": "23277b9ec325e43b6e5a61810ad24fe7",
+    "url": "/CartUser_web/index.html"
+  }
+];
